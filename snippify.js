a39056f94d6db1186cac02363c03ebdf7638a18e
Aaron Putterman <abp6318@rit.edu>
2023-10-22 00:34:00 -0400

    feat: added snippify script

---
+++ snippify.js
@@ -0,0 +1,82 @@
+// TODO: Add branching inputs for sending snippets to separate files; maybe branch off for full and partial snippets 
+const fs = require('fs');
+const prompts = require('prompts');
+
+(async () => {
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'snippetName',
+            message: 'Name for snippet',
+            validate: text => text !== ""
+        }, 
+        {
+            type: 'text',
+            name: 'prefix',
+            message: 'Prefix for snippet',
+            validate: text => text !== ""
+        }, 
+        {
+            type: 'text',
+            name: 'description',
+            message: 'Description for snippet',
+            validate: text => text !== ""
+        }
+    ]);
+
+    processSnippet(response);
+})();
+
+function processSnippet(args) {
+    const { snippetName, prefix, description } = args;
+    const filePath = 'snippet.txt';
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw err;
+        const snippet = snippify(prefix, description, data);
+        addSnippetToList(snippetName, snippet);
+    });
+}
+
+function snippify(prefix, description, codeBlock) {
+    // Split the code block into lines
+    const lines = codeBlock.split("\n");
+
+    // Convert tabs to "\t" in each line
+    const convertedLines = lines.map(
+    line => line
+        .replace(/\t/g, "\t")
+        .replace("    ", "\t")
+        .replace("  ", "\t")
+    );
+
+    const jsonArray = JSON.stringify(convertedLines);
+
+    const snippet = {
+        prefix,
+        description,
+        body: jsonArray
+    };
+
+    return snippet;
+}
+
+function addSnippetToList(snippetName, newSnippet) {
+    const snippetFile = 'snippets/snippets.code-snippets';
+    fs.readFile(snippetFile, 'utf8', (err, data) => {
+        if (err) throw err;
+        
+        // read in existing snippets
+        let currentSnippets = JSON.parse(data);
+
+        // add new snippet
+        currentSnippets[snippetName] = newSnippet
+
+        const updatedSnippets = JSON.stringify(currentSnippets, null, 2);
+
+        // write to file
+        fs.writeFile(snippetFile, updatedSnippets, 'utf8', (err) => {
+            if (err) throw err;
+        });
+    });
+}
